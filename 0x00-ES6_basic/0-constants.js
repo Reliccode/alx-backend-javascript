3c83874fa0a424ce572e6f1fe285c3d044be5616
Reliccode <bryanmaritim@gmail.com>
2024-01-02 21:34:31 +0300

    const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+//modify taskFirst to use const
+//modify taskNext to use let
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
