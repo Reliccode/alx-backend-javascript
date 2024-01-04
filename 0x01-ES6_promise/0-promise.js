98d9b1a7975bbb8fe71f472b361b604b1ee8ef65
Reliccode <bryanmaritim@gmail.com>
2024-01-04 13:10:33 +0300

    fix: Setting promise rejection reason to be Error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const gotResponse = true;
-        if (gotResponse) {
-            resolve('true');
-        } else {
-            reject('false');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const gotResponse = true;
+    if (gotResponse) {
+      resolve('true');
+    } else {
+      reject(new Error('false'));
+    }
+  });
 }
