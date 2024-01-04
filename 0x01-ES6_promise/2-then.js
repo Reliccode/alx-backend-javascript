abd465f022e1a540dcafde2259964e39ccc49694
Reliccode <bryanmaritim@gmail.com>
2024-01-04 19:15:30 +0300

    fix: removing unused vars, working with no vars

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((result) => {
-      /* eslint-disable-line no-unused-vars */
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
-      /* eslint-disable-line no-unused-vars */
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
