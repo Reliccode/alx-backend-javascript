0a4f1ac5f3dc0d125907747e330a709443d9e692
Reliccode <bryanmaritim@gmail.com>
2024-02-27 17:22:47 +0300

    fix: docstrings

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,10 @@
-function displayMessage(message){
-    console.log(message);
+/**
+*Function that displays message
+*@param message The message to display
+* */
 
+function displayMessage(message) {
+  console.log(message);
 }
 
 module.exports = displayMessage;
