367cf017290297ce7b0bf557fce1ebd884645a40
Reliccode <bryanmaritim@gmail.com>
2024-02-29 17:30:54 +0300

    Calculates sum of two rounded numbers

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates sum of two rounded nums
+ * @param {number} a - The first number.
+ * @param {number} b - Second num
+ * @returns {number} Sum of rounded nums
+ */
+
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+}
+
+
+module.exports = calculateNumber;
